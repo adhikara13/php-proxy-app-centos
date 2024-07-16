56c4cb4aeff374e2cdaa022fc22e8805718645ab
root <root@DESKTOP-J8GQQO7>
2024-07-16 13:34:21 +0300

    Made the design way better

--- templates/main.php
+++ templates/main.php
@@ -1,90 +1,106 @@
 <!DOCTYPE html>
-<html>
+<html lang="en">
 <head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Free Web Proxy from ProxyMan</title>
+    <meta name="generator" content="php-proxy.com">
+    <meta name="version" content="<?=$version;?>">
 
-<title>PHP-Proxy</title>
+    <style>
+        body {
+            margin: 0;
+            font-family: Arial, Helvetica, sans-serif;
+            background-color: #f4f4f4;
+            display: flex;
+            flex-direction: column;
+            align-items: center;
+            justify-content: center;
+            height: 100vh;
+            color: #333;
+        }
 
-<meta name="generator" content="php-proxy.com">
-<meta name="version" content="<?=$version;?>">
+        #container {
+            background-color: #fff;
+            padding: 30px 40px;
+            box-shadow: 0 4px 8px rgba(0, 0, 0, 0.1);
+            border-radius: 10px;
+            text-align: center;
+        }
 
-<style type="text/css">
-html body {
-	font-family: Arial,Helvetica,sans-serif;
-	font-size: 12px;
-}
+        #frm {
+            margin-top: 20px;
+        }
 
-#container {
-	width:500px;
-	margin:0 auto;
-	margin-top:150px;
-}
+        #frm input[type="text"] {
+            width: calc(100% - 90px);
+            padding: 10px;
+            margin-right: 10px;
+            border: 1px solid #ccc;
+            border-radius: 5px;
+        }
 
-#error {
-	color:red;
-	font-weight:bold;
-}
+        #frm input[type="submit"] {
+            padding: 10px 20px;
+            border: none;
+            background-color: #007bff;
+            color: white;
+            border-radius: 5px;
+            cursor: pointer;
+        }
 
-#frm {
-	padding:10px 15px;
-	background-color:#FFC8C8;
-	
-	border:1px solid #818181;
-	
-	-webkit-border-radius: 8px;
-	-moz-border-radius: 8px;
-	border-radius: 8px;
-}
+        #frm input[type="submit"]:hover {
+            background-color: #0056b3;
+        }
 
-#footer {
-	text-align:center;
-	font-size:10px;
-	margin-top:35px;
-	clear:both;
-}
-</style>
+        #error {
+            color: red;
+            font-weight: bold;
+            margin-top: 20px;
+        }
 
+        #footer {
+            margin-top: 30px;
+            font-size: 12px;
+            color: #777;
+        }
+
+        #footer a {
+            color: #007bff;
+            text-decoration: none;
+        }
+
+        #footer a:hover {
+            text-decoration: underline;
+        }
+    </style>
 </head>
-
 <body>
 
-
 <div id="container">
+    <h1 style="color: #007bff;">Free Web Proxy by ProxyMan</h1>
 
-	<div style="text-align:center;">
-		<h1 style="color:blue;">PHP-Proxy</h1>
-	</div>
-	
-	<?php if(isset($error_msg)){ ?>
-	
-	<div id="error">
-		<p><?php echo strip_tags($error_msg); ?></p>
-	</div>
-	
-	<?php } ?>
-	
-	<div id="frm">
-	
-	<!-- I wouldn't touch this part -->
-	
-		<form action="index.php" method="post" style="margin-bottom:0;">
-			<input name="url" type="text" style="width:400px;" autocomplete="off" placeholder="http://" />
-			<input type="submit" value="Go" />
-		</form>
-		
-		<script type="text/javascript">
-			document.getElementsByName("url")[0].focus();
-		</script>
-		
-	<!-- [END] -->
-	
-	</div>
-	
+    <?php if(isset($error_msg)){ ?>
+    <div id="error">
+        <p><?php echo strip_tags($error_msg); ?></p>
+    </div>
+    <?php } ?>
+
+    <div id="frm">
+        <form action="index.php" method="post" style="margin-bottom:0;">
+            <input name="url" type="text" autocomplete="off" placeholder="http://">
+            <input type="submit" value="Go">
+        </form>
+
+        <script type="text/javascript">
+            document.getElementsByName("url")[0].focus();
+        </script>
+    </div>
 </div>
 
 <div id="footer">
-	Powered by <a href="//www.php-proxy.com/" target="_blank">PHP-Proxy</a> <?php echo $version; ?>
+    Powered by <a href="//komodomain/" target="_blank">ProxyMan</a> <?php echo $version; ?>
 </div>
-
 
 </body>
 </html>
--- templates/url_form.php
+++ templates/url_form.php
@@ -1,77 +1,95 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>PHP-Proxy</title>
+    <meta name="generator" content="php-proxy.com">
+    <meta name="version" content="<?=$version;?>">
 
-<style type="text/css">
+    <style>
+        body {
+            margin: 0;
+            font-family: Arial, Helvetica, sans-serif;
+            background-color: #f4f4f4;
+            color: #333;
+        }
 
-html body {
-	margin-top: 50px !important;
-}
+        #top_form {
+            position: fixed;
+            top: 0;
+            left: 0;
+            width: 100%;
+            margin: 0;
+            z-index: 2100000000;
+            background: #007bff;
+            border-bottom: 1px solid #0056b3;
+            height: 50px;
+            line-height: 50px;
+            display: flex;
+            justify-content: center;
+            align-items: center;
+            box-shadow: 0 4px 8px rgba(0, 0, 0, 0.1);
+        }
 
-#top_form {
-	position: fixed;
-	top:0;
-	left:0;
-	width: 100%;
-	
-	margin:0;
-	
-	z-index: 2100000000;
-	-moz-user-select: none; 
-	-khtml-user-select: none; 
-	-webkit-user-select: none; 
-	-o-user-select: none; 
-	
-	border-bottom:1px solid #151515;
-	
-    background:#FFC8C8;
-	
-	height:45px;
-	line-height:45px;
-}
+        #top_form input[type="text"] {
+            width: 550px;
+            height: 35px;
+            padding: 5px 10px;
+            font-size: 14px;
+            border: 1px solid #ccc;
+            border-radius: 5px;
+            margin-right: 10px;
+        }
 
-#top_form input[name=url] {
-	width: 550px;
-	height: 20px;
-	padding: 5px;
-	font: 13px "Helvetica Neue",Helvetica,Arial,sans-serif;
-	border: 0px none;
-	background: none repeat scroll 0% 0% #FFF;
-}
+        #top_form input[type="submit"],
+        #top_form input[type="button"] {
+            padding: 10px 20px;
+            border: none;
+            background-color: #0056b3;
+            color: white;
+            border-radius: 5px;
+            cursor: pointer;
+        }
 
-</style>
+        #top_form input[type="submit"]:hover,
+        #top_form input[type="button"]:hover {
+            background-color: #003d80;
+        }
+    </style>
 
-<script>
-var url_text_selected = false;
+    <script>
+        var url_text_selected = false;
 
-function smart_select(ele){
+        function smart_select(ele) {
+            ele.onblur = function() {
+                url_text_selected = false;
+            };
 
-	ele.onblur = function(){
-		url_text_selected = false;
-	};
-	
-	ele.onclick = function(){
-		if(url_text_selected == false){
-			this.focus();
-			this.select();
-			url_text_selected = true;
-		}
-	};
-}
-</script>
+            ele.onclick = function() {
+                if (url_text_selected == false) {
+                    this.focus();
+                    this.select();
+                    url_text_selected = true;
+                }
+            };
+        }
+    </script>
+</head>
+<body>
 
 <div id="top_form">
-
-	<div style="width:800px; margin:0 auto;">
-	
-		<form method="post" action="index.php" target="_top" style="margin:0; padding:0;">
-			<input type="button" value="Home" onclick="window.location.href='index.php'">
-			<input type="text" name="url" value="<?php echo $url; ?>" autocomplete="off">
-			<input type="hidden" name="form" value="1">
-			<input type="submit" value="Go">
-		</form>
-		
-	</div>
-	
+    <form method="post" action="index.php" target="_top" style="margin:0; padding:0; display: flex; align-items: center;">
+        <input type="button" value="Home" onclick="window.location.href='index.php'">
+        <input type="text" name="url" value="<?php echo $url; ?>" autocomplete="off">
+        <input type="hidden" name="form" value="1">
+        <input type="submit" value="Go">
+    </form>
 </div>
 
 <script type="text/javascript">
-	smart_select(document.getElementsByName("url")[0]);
+    smart_select(document.getElementsByName("url")[0]);
 </script>
+
+</body>
+</html>
